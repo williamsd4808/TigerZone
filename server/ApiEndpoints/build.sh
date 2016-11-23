8c69d247563f3265a901c3223453d568fa0a0404
Austin Seber <aseber@techsouth.cc>
2016-11-23 18:20:12 -0500

    More work...

--- server/ApiEndpoints/build.sh
+++ server/ApiEndpoints/build.sh
@@ -1,10 +1,10 @@
 javac -cp ".:../../lib/javax.json-1.0.jar" -sourcepath ".:../" NewGame.java
-# javac -cp ".:../../lib/javax.json-1.0.jar" -sourcepath ".:../" JoinGame.java
-# javac -cp ".:../../lib/javax.json-1.0.jar" -sourcepath ".:../" GetMoves.java
-# javac -cp ".:../../lib/javax.json-1.0.jar" -sourcepath ".:../" PlaceTile.java
+javac -cp ".:../../lib/javax.json-1.0.jar" -sourcepath ".:../" JoinGame.java
+javac -cp ".:../../lib/javax.json-1.0.jar" -sourcepath ".:../" GetMoves.java
+javac -cp ".:../../lib/javax.json-1.0.jar" -sourcepath ".:../" PlaceTile.java
 
-# chmod +x new-game
-# chmod +x join-game
-# chmod +x get-moves
-# chmod +x place-tile
-# chmod +x watch-game
+chmod +x new-game
+chmod +x join-game
+chmod +x get-moves
+chmod +x place-tile
+chmod +x watch-game
