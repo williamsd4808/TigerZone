4f0f5f98b41c42b4144661ed688e87e6963c442d
rickydtran <rickydtran@ufl.edu>
2016-12-01 01:35:18 -0500

    java server and client

--- server/Main.java
+++ server/Main.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class Main {
 	public enum Token {
-		CONNECTED, AUTH, WAIT, 
+		// AUTH, CHALLENGE, ROUND, MATCH, MOVE, UNKNOWN;
+		TPASS, AUTH, WELCOME, NEW, EOC, ROUND, EOR, OPPONENT, STILE, DECK, MOVE, GAME, GAMEOVER, UNKNOWN;
 	}
 	public static void main(String[] args) throws IOException {
 		if(args.length == 0) {
@@ -12,7 +13,6 @@ public class Main {
 			return;
 		}
 		String address = args[0];
-		int foo = Integer.parseInt("1234");
 		int port = Integer.parseInt(args[1]);
 		String tpass = args[2];
 		String uname = args[3];
@@ -30,67 +30,99 @@ public class Main {
 			System.exit(1);
 		}
 		//Begin parsing server data
+		boolean done = false;
 		String input, fromServer;
+		String pid1, pid2;
+		int cid, rounds, rid;
+		cid = 0;
+		rounds = 0;
+		rid = 0;
 		BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));			
 		//Scanner scan = new Scanner(System.in);	
 		try {
-			fromServer = in.readLine();
-			if(fromServer.startsWith("THIS")) {
-				System.out.println("Connected");
-			}
-			while(true) {
-
+			while(done != true) {
+				Token token = Token.UNKNOWN;				
 				fromServer = in.readLine();
-				if(fromServer.startsWith("HELLO!")) {
-				}//do nothing
-				else if(fromServer.startsWith("WELCOME")) {
-					break;
-				}
-				else if(fromServer.startsWith("NEW")) {
-					break;
-				}
-				else if(fromServer.startsWith("BEGIN")) {
-					break;
-				}
-				else if(fromServer.startsWith("YOUR")) {
-					break;
-				}
-				else if(fromServer.startsWith("STARTING")) {
-					break;
-				}
-				else if(fromServer.startsWith("THE")) {
-					break;
-				}
-				else if(fromServer.startsWith("MAKE")) {
-					break;
-				}
-				else if(fromServer.startsWith("GAME")) {
-					break;
-				}									
-				else if(fromServer.startsWith("END")) {
-					break;
-				}								
-				else if(fromServer.startsWith("END")) {
-					break;
-				}									
-				else if(fromServer.startsWith("END")) {
-					break;
-				}								
-				else if(fromServer.startsWith("PLEASE")) {
-					break;
-				}											
-				else if(fromServer.startsWith("THANK")) {
-					break;
-				}
-				else {
-					System.out.println("Invalid Buffer Data from Server or perhaps crash?");
-					return;
-				}
-				//BASED OFF TOKEN DO SOMETHING
-
-				
+				System.out.println(fromServer);
+				token = getToken(fromServer);
+				switch(token) {
+					case TPASS:
+						out.println("JOIN " + tpass);
+						break;
+					case AUTH:
+						out.println("I AM " + uname + " " + upass);
+						break;
+					case WELCOME:
+						//GET PID
+    					String[] sp = fromServer.split(" ");
+    					pid1 = sp[1];
+    					// System.out.println(pid1); //test
+    					//SPIN LOCK TILL WAIT TOKEN IS RECEIVED
+    					while(true) {
+    						token = Token.UNKNOWN;
+    						fromServer = in.readLine();
+							// System.out.println(fromServer);    						
+    						token = getToken(fromServer);
+    						if(token == Token.NEW) {
+    							String[] sp2 = fromServer.split(" ");
+    							cid = Integer.parseInt(sp2[2]);
+    							rounds = Integer.parseInt(sp2[6]);
+    							//test parsing data stuff
+    							// System.out.println(rid + " " + rounds);
+    							break;
+    						}
+    					}
+    					break;
+    				case EOC:
+    					//chances are 1 challenge really do nothing
+    					break;
+					case ROUND:
+						String sp3[] = fromServer.split(" ");
+						rid = Integer.parseInt(sp3[2]);
+						//CREATE TWO NEW GAMES 
+						break;
+					case EOR:
+						//increment counter. if less than equal to rounds then wait
+						rid++;
+						if(rid > rounds) {
+							//spinlock till next match
+	    					while(true) {
+	    						token = Token.UNKNOWN;
+	    						fromServer = in.readLine();
+								System.out.println(fromServer);    						
+	    						token = getToken(fromServer);
+	    						if(token == Token.ROUND) {
+									String sp4[] = fromServer.split(" ");
+									rid = Integer.parseInt(sp4[2]);
+	    							//test parsing data stuff
+	    							// System.out.println(rid + " " + rounds);
+	    							break;
+	    						}
+	    					}
+	    					break;							
+						}
+					case OPPONENT:
+						String sp5[] = fromServer.split(" ");
+						pid2 = sp5[4];
+						//add player to each game
+						break;
+					case STILE:
+						//DO NOTHING. Board already does it on new game
+						break;
+					case DECK:
+						//COPY DECK TO THERE DECK.
+						break;
+					case MOVE:
+						//do valid move from our AI;
+						break;
+					case GAME:
+						//translate game data;
+						break;
+					case GAMEOVER:
+						done = true;
+						break;
+				}	
 			}
-			out.println("Bye.");
 		}
 		finally {
 			in.close();			
@@ -98,5 +130,47 @@ public class Main {
 			// buffer.close();
 			socket.close();
 		}
+	}
+	public static Token getToken(String fromServer) {
+		
+		if(fromServer.startsWith("THIS")) {
+			return Token.TPASS;
+		}
+		else if(fromServer.startsWith("HELLO!")) {
+			return Token.AUTH;
+		}
+		else if(fromServer.startsWith("WELCOME")) {
+			return Token.WELCOME;
+		}
+		else if(fromServer.startsWith("NEW")) {
+			return Token.NEW;
+		}
+		else if(fromServer.startsWith("BEGIN")) {
+			return Token.ROUND;
+		}
+		else if(fromServer.startsWith("YOUR")) {
+			return Token.OPPONENT;
+		}
+		else if(fromServer.startsWith("STARTING")) {
+			return Token.STILE;
+		}
+		else if(fromServer.startsWith("THE")) {
+			return Token.DECK;
+		}
+		else if(fromServer.startsWith("MAKE")) {
+			return Token.MOVE;
+		}
+		else if(fromServer.startsWith("GAME")) {
+			return Token.GAME;
+		}									
+		else if(fromServer.startsWith("END")) {
+			return Token.EOR;
+		}																		
+		else if(fromServer.startsWith("THANK")) {
+			return Token.GAMEOVER;
+		}
+		else {
+			return Token.UNKNOWN;
+		}		
 	}
 }
